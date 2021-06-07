68868d850606bbfbfa1c17247b9784392850c4b1
MarcoPEA <73260782+MarcoPEA@users.noreply.github.com>
2021-06-06 21:28:48 -0500

    Sip

---
+++ app/src/main/java/com/example/aplicacionsip/MainActivity.java
@@ -0,0 +1,218 @@
+package com.example.aplicacionsip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.sip.SipAudioCall;
+import android.net.sip.SipException;
+import android.net.sip.SipManager;
+import android.net.sip.SipProfile;
+import android.net.sip.SipRegistrationListener;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.ParseException;
+
+public class MainActivity extends AppCompatActivity {
+    private SipManager sipManager = null;
+    private SipProfile sipProfile = null;
+    private SipProfile.Builder builder = null;
+
+    SipAudioCall call = null;
+    public IncomingCallReceiver callReceiver;
+
+    String username = "telefono1";
+    String domain = "sip.antisip.com";
+    String password = "llamar";
+
+    Button btnLlamar, btnResponder;
+    boolean veri = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(SipManager.isVoipSupported(getApplicationContext())){
+            Log.d("VOIP:", "Supported!");
+        }
+        else{
+            Log.d("VOIP:", "Not Supported");
+        }
+        if(SipManager.isApiSupported(getApplicationContext())){
+            Log.d("API:", "Supported!");
+        }
+        else{
+            Log.d("API:","NotSupported!");
+        }
+
+        btnLlamar = findViewById(R.id.btnLlamar);
+        btnLlamar.setOnClickListener(v->{
+            try {
+                SipAudioCall call = sipManager.makeAudioCall(sipProfile.getUriString(), "sip:telefono2@sip.antisip.com", listener, 30);
+                Log.d("CALL", call.getState()+"");
+            } catch (SipException e) {
+                e.printStackTrace();
+            }
+        });
+
+        btnResponder = findViewById(R.id.btnResponder);
+        btnResponder.setOnClickListener(v->{
+            if(veri){
+                btnResponder.setText("Colgar");
+                veri = false;
+            }else {
+                btnResponder.setText("Contestar");
+                veri = true;
+            }
+            callReceiver.accept();
+        });
+
+        //INICIARLIZAR SIP MANAGER
+        if (sipManager == null) {
+            sipManager = SipManager.newInstance(this);
+        }
+
+        //CONSTRUIR EL PERFIL SIP
+        try {
+            builder = new SipProfile.Builder(username, domain);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        builder.setPassword(password);
+        sipProfile = builder.build();
+
+        /**Abre el perfil local para realizar o recibir llamadas SIP genéricas**/
+        Intent intent = new Intent();
+        intent.setAction("com.example.aplicacionsip.INCOMING_CALL");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, Intent.FILL_IN_DATA);
+        try {
+            sipManager.open(sipProfile, pendingIntent, null);
+        } catch (SipException e) {
+            e.printStackTrace();
+        }
+
+        /** Realiza un seguimiento de si el SipProfile se registró correctamente con tu proveedor de servicios SIP **/
+        try {
+            sipManager.setRegistrationListener(sipProfile.getUriString(), new SipRegistrationListener() {
+                @Override
+                public void onRegistering(String s) {
+                    Log.d("HERA", "Registering with SIP Server...");
+                }
+
+                @Override
+                public void onRegistrationDone(String s, long l) {
+                    Log.d("HERA", "Ready" + s);
+                }
+
+                @Override
+                public void onRegistrationFailed(String s, int i, String s1) {
+                    Log.d("HERA", "Registration failed.  Please check settings.");
+                }
+            });
+        } catch (SipException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Filtro que intercepta la transmisión y activa el receptor (IncomingCallReceiver)
+         */
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.example.aplicacionsip.INCOMING_CALL");
+        callReceiver = new IncomingCallReceiver();
+        this.registerReceiver(callReceiver, filter);
+    }
+    SipAudioCall.Listener listener = new SipAudioCall.Listener() {
+
+        @Override
+        public void onCallEstablished(SipAudioCall call) {
+            Log.d("CALL", "Call started!");
+            call.setSpeakerMode(true);
+            call.startAudio();
+        }
+
+        @Override
+
+        public void onCallEnded(SipAudioCall call) {
+            Log.d("CALL", "END");
+            //closeLocalProfile();
+
+        }
+    };
+    public void closeLocalProfile() {
+        if (sipManager == null) {
+            return;
+        }
+        try {
+            if (sipProfile != null) {
+                sipManager.close(sipProfile.getUriString());
+            }
+        } catch (Exception ee) {
+            Log.d("HERA", "Failed to close local profile.");
+        }
+    }
+    public static class IncomingCallReceiver extends BroadcastReceiver {
+        SipAudioCall incomingCall = null;
+        Vibrator v = null;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals("com.example.aplicacionsip.INCOMING_CALL")){
+                Toast.makeText(context, "Llamada entrante ", Toast.LENGTH_LONG).show();
+                v = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(3000);
+            }
+            try {
+                SipAudioCall.Listener listener = new SipAudioCall.Listener() {
+                    @Override
+                    public void onRinging(SipAudioCall call, SipProfile caller) {
+                        try {
+                            call.answerCall(30);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                MainActivity wtActivity = (MainActivity) context;
+                incomingCall = wtActivity.sipManager.takeAudioCall(intent, listener);
+                wtActivity.call = incomingCall;
+                if(incomingCall.isMuted()) {
+                    incomingCall.toggleMute();
+                }
+//                wtActivity.updateStatus(incomingCall);
+            } catch (Exception e) {
+                if (incomingCall != null) {
+                    incomingCall.close();
+                }
+            }
+        }
+        boolean ver = true;
+        public void accept()
+        {
+            Log.d("Receiver","Answer");
+            v.cancel();
+            try {
+                if(ver){
+                    incomingCall.answerCall(30);
+                    incomingCall.startAudio();
+                    incomingCall.setSpeakerMode(true);
+                    ver = false;
+                }else{
+                    incomingCall.close();
+                    ver = true;
+                }
+            } catch (SipException e) {
+                Log.d("Call Exception",e.toString());
+                e.printStackTrace();
+            }
+            catch (Exception e)
+            {
+                Log.d("Exception",e.toString());
+            }
+        }
+    }
+}
